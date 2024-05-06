568e3d9af2eee78eaa0644a1a05e4070c9797795
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-05-07 10:19:54 +1200

    Update local symlinks for new mount

--- local_setup.sh
+++ local_setup.sh
@@ -3,4 +3,5 @@
 # This file sets up symlinks locally so components can be used.
 # On CloudCannon, this will use Site Mounting.
 
-[ ! -e component-library ] && ln -s ../component-library component-library
+[ ! -e component-library ] && ln -s ../component-library/component-library component-library
+[ ! -e site/_includes/layouts ] && ln -s ../../../component-library/layouts site/_includes/layouts
