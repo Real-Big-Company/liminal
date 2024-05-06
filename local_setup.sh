bb3e4f13888109e61acd58b65dd79a95a67ef511
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-05-07 10:36:13 +1200

    Update local symlinks

--- local_setup.sh
+++ local_setup.sh
@@ -3,5 +3,5 @@
 # This file sets up symlinks locally so components can be used.
 # On CloudCannon, this will use Site Mounting.
 
-[ ! -e component-library ] && ln -s ../component-library/component-library component-library
-[ ! -e site/_includes/layouts ] && ln -s ../../../component-library/layouts site/_includes/layouts
+[ ! -e component-library ] && ln -s ../common/component-library component-library
+[ ! -e site/_includes/layouts ] && ln -s ../../../common/layouts site/_includes/layouts
